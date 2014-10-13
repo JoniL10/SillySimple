bf15cc5a80c30d0d6818a0326872b2f0b03172c7
JoniL10 <joni_10@suomi24.fi>
2014-10-13 22:28:30 +0300

    kommit

---
+++ getuser.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$q = intval($_GET['q']);
+
+$con = mysqli_connect('db4free.net','joni10','botnia','joni10');
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"testitaulu");
+$result = mysqli_query($con,"SELECT * FROM testitaulu WHERE id=$q");
+
+while($row = mysqli_fetch_array($result)) {
+  echo $row['nimi'];
+  echo ', ';
+ echo $row['ika'];
+}
+
+
+mysqli_close($con);
+?> 
